5c8805c29f373e7d814e3baf8c26a4a68c2eb37d
ChiragCD <Chirag8CD@gmail.com>
2019-10-13 16:23:49 +0530

    Update sublist.pl, with minor changes
    Changed Or to conditional operator for readability.

---
+++ sublist.pl
@@ -0,0 +1,9 @@
+% __________ sublist( X, Y)
+% sublist( X, Y) checks if list X is a subset of list Y
+% subset - all X elements exist in Y, in the same order.
+
+sublist([], _).				% base case - since empty list is subset of any term. X has been exhausted.
+sublist([X|X_Cont], [Y|Y_Cont]):-	% search through Y till leading element in X matches, then pop both and repeat.
+    X=:=Y ->	
+    sublist(X_Cont, Y_Cont);		% if match found, pop both and repeat.
+    sublist([X|X_Cont], Y_Cont).	% else, continue through Y.
