ea1660dd9eb18db1de848b43d3e60edc86a4567e
ChiragCD <Chirag8CD@gmail.com>
2019-10-13 17:32:35 +0530

    Fill has_triplicate

--- has_triplicate.pl
+++ has_triplicate.pl
@@ -1,12 +1,16 @@
-has_triplicate([]):-false.
-has_triplicate([_]):-false.
-has_triplicate([_, _]):-false.
+% _________ has_triplicate( X) _________
+% Check if X has 3 or more copies of any element, print element if so.
 
-has_triplicate([X|L]):-
-    delete_all(X, L, New_L, C),
-    (C>1 ->  (write(X), nl);true),
-    has_triplicate(New_L).
+has_triplicate([]):-false.		% Base Case - X has been exhausted.
 
-delete_all(_, [], [], 0).
-delete_all(X, [H|Y], Z, C):- X=:=H ->  (delete_all(X, Y, Z, C_inner) ->  C is C_inner+1).
-delete_all(X, [Y|Y_Cont], [Y|Z_Cont], C):- X=\=Y ->  delete_all(X, Y_Cont, Z_Cont, C).
+has_triplicate([X|L]):-			% Logic - For each element,
+    delete_all(X, L, New_L, C),		% count and delete all further occurrences.
+    (C>1 ->  (write(X), nl);true),	% If count shows triplicate, print, else continue anyway.
+    has_triplicate(New_L).		% Check for remaining elements.
+
+% _________ delete_all( X, Y, Z, C) _________
+% Delete all occurrences of X from Y, store the result in Z. Also count occurrences in C.
+
+delete_all(_, [], [], 0).									% Base Case - Y has been completely parsed.
+delete_all(X, [H|Y], Z, C):- X=:=H ->  (delete_all(X, Y, Z, C_inner), C is C_inner+1).		% If occurrence found, increment C, skip X, continue.
+delete_all(X, [Y|Y_Cont], [Y|Z_Cont], C):- X=\=Y ->  delete_all(X, Y_Cont, Z_Cont, C).		% If not an occurrence, continue.
