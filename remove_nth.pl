c863ca893c8d2ad7de291d33ed51768cb4fe2f57
ChiragCD <Chirag8CD@gmail.com>
2019-10-13 17:05:43 +0530

    Correct remove_nth
    X and Y were switched in the old version.

--- remove_nth.pl
+++ remove_nth.pl
@@ -1,14 +1,16 @@
 % __________  remove_nth( N, X, Y)  ______________
-% remove_nth( N, X, Y) removes the element at the Nth index and stores the list in Y 
+% remove_nth( N, X, Y) removes the element at the Nth index of X and stores the list in Y 
 % returns false/no (version dependent) if N exceeds the length 
 
-remove_nth(1, X, [_|Y]):-
+% Base case, if index N is reached, skip over that element in X.
+remove_nth(1, [_|X], Y):-
     equals(X, Y).
 
+% Until base case, retain X elements in Y.
 remove_nth(N, [H|X], [H|Y]):-
     M is N-1,
     remove_nth(M, X, Y).
 
-% equals( A, B) stores the list B in A  
+% equals( A, B) stores the list B in A, by comparing for equality.
 equals([], []).
 equals([H|Y], [H|X]):-equals(Y, X).
