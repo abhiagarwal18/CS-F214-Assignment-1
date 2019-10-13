8a8e1845f1c5ae158ec1b7d03396aaaf982449b9
ChiragCD <Chirag8CD@gmail.com>
2019-10-13 17:07:43 +0530

    Correct naming issue, code error
    Again, switch X and Y.

---
+++ remove_every_other.pl
@@ -0,0 +1,11 @@
+% _________ remove_every_other( X, Y) __________
+% stores every alternate element of X in Y beginning with the first element
+
+remove_every_other([], []).				% Base Case
+remove_every_other([H|X], [H|Y]):- other(X, Y).		% Keep 1st element
+
+% _________ other( X, Y) __________
+% stores every alternate element of X in Y beginning with the second element
+
+other([], []).						% Base Case
+other([_|X], Y):- remove_every_other(X, Y).		% Skip over the 1st element
